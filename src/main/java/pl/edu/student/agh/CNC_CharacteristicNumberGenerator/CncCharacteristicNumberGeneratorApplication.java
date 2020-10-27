20c86e0e485b22a7e90dc5540e35348e4541308d
Jerzy Pałka <57533018+kudini@users.noreply.github.com>
2020-10-27 18:37:25 +0100

    spring boot

---
+++ src/main/java/pl/edu/student/agh/CNC_CharacteristicNumberGenerator/CncCharacteristicNumberGeneratorApplication.java
@@ -0,0 +1,13 @@
+package pl.edu.student.agh.CNC_CharacteristicNumberGenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CncCharacteristicNumberGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CncCharacteristicNumberGeneratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/edu/student/agh/CNC_CharacteristicNumberGenerator/CncCharacteristicNumberGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.edu.student.agh.CNC_CharacteristicNumberGenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CncCharacteristicNumberGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
